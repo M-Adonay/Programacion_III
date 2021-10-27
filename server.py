625346161714ed5988290eabd499bbc6c6693895
Mauricio Adonay Espinoza Villatoro <usis018120@ugb.edu.sv>
2021-10-27 12:41:09 -0600

    Avance de la clase

--- server.py
+++ server.py
@@ -2,6 +2,22 @@ from http import server
 from http.server import BaseHTTPRequestHandler, HTTPServer, SimpleHTTPRequestHandler
 from urllib import parse
 import json
+import mysql.connector
+from mysql.connector import Error
+
+class crud:
+    def __init__(self):
+        print("Iniciando conexion con la base de datos...")
+        self.db = mysql.connector.connect(
+            host ="localhost",
+            user ="root",
+            passwd = "",
+            database = "db_academica_a2"
+        )
+        if self.db.is_connected():
+            print("Conexion establecida")
+        else:
+            print("Conexion Fallida")
 
 class servidorBasico(SimpleHTTPRequestHandler):
     def do_GET(self):
