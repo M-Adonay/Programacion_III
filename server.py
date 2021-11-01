6794006ba3fb911911b8b6118f64ac9b5783646f
Mauricio Adonay Espinoza Villatoro <usis018120@ugb.edu.sv>
2021-11-01 15:48:36 -0600

    Avance

--- server.py
+++ server.py
@@ -18,12 +18,30 @@ class crud:
             print("Conexion establecida")
         else:
             print("Conexion Fallida")
+    
+    def insertar(self, codigo, nombre, telefono):
+        cursor = self.db.cursor()
+        sql = "INSERT INTO alumnos (codigo, nombre, telefono) VALUES (%s, %s, %s)"
+        val = (codigo, nombre, telefono)
+        cursor.execute(sql, val)
+        self.db.commit()
+        return "Registro insertado"
+crud = crud()
+
 
 class servidorBasico(SimpleHTTPRequestHandler):
     def do_GET(self):
         if self.path == "/":
             self.path = "/index.html"
         return SimpleHTTPRequestHandler.do_GET(self)
+
+    def do_Post(self):
+        longitud_contenido = int(self.headers['Content-Length'])
+        contenido = self.rfile.read(longitud_contenido)
+        contenido = contenido.decode("utf-8")
+        contenido = parse.unquote(contenido)
+        contenido = json.loads(contenido)
+        
 
 print ("Servidor Iniciado")
 server = HTTPServer(("localhost", 3000), servidorBasico)
